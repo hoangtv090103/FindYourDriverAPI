1ca7bd9dbcfcc0efc2c27fc813e253da42fa0d2d
hoangtv090103 <hoangtv090103@gmail.com>
2024-03-31 12:02:56 +0700

    Refactor userRoute.js routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,8 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
-router.post('/add', userController.addUser);
-router.put('/update/:id', userController.updateUser);
-router.delete('/delete/:id', userController.deleteUser);
+router.post('/', userController.addUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
